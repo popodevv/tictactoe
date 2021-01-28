0d83546a453f898d8c6e8b109b24131d60f0cf88
Delmotte Pauline <paulinedelmotte63@gmail.com>
2021-01-28 09:58:20 +0100

    structure html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParamPageComponent } from './param-page/param-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParamPageComponent,
+    GamePageComponent
   ],
   imports: [
     BrowserModule,
